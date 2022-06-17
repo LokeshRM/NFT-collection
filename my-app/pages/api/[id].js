c2715252a4b91af33d6cff77c94b99c8b4e2512f
LokeshReddyMeda <lokeshreddy.meda@gmail.com>
2022-06-17 22:21:00 +0530

    Update [id].js

--- my-app/pages/api/[id].js
+++ my-app/pages/api/[id].js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
   const { id } = req.query;
-  const image_url = "https://crypto-rain.netlify.app/api/images/";
+  const image_url = `https://crypto-rain.netlify.app/img (${id}).png`;
   res.status(200).json({
     name: "Crypto-Rain #" + id,
     description: "Nft collection for the developers",
-    image: image_url + id,
+    image: image_url,
   });
 }
